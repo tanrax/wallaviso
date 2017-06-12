fa54fa0021f075998f570cfe37c99675a86e6d20
Andros Fenollosa <andros@fenollosa.email>
2017-06-12 23:37:25 +0200

    Update google analytics

--- static/js/main.js
+++ static/js/main.js
@@ -1,27 +1,22 @@
-$(function () {
-	// Navbar fixed animation
-	$(".navbar-fixed-top").autoHidingNavbar({});
+$(function() {
+    // Navbar fixed animation
+    $(".navbar-fixed-top").autoHidingNavbar({});
 
-	// Select2
-	//$('select').select2();
-$("#mySelect").on("select2-open", function() { 
-    $("#mySelect").select2("close");
-    openNativeSelect($('#mySelect'));
-});
+    // Alerts
+    vex.dialog.buttons.YES.text = 'Vale';
 
-	// Alerts
-	vex.dialog.buttons.YES.text = 'Vale';
-
-	// Tooltips
-	$('[data-toggle="tooltip"]').tooltip();
-
-	// Google analytics
-	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-		  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-		  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-		  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-		  ga('create', 'UA-100374882-1', 'auto');
-		  ga('send', 'pageview');
-
+    // Google analytics
+    (function(i, s, o, g, r, a, m) {
+        i['GoogleAnalyticsObject'] = r;
+        i[r] = i[r] || function() {
+            (i[r].q = i[r].q || []).push(arguments)
+        }, i[r].l = 1 * new Date();
+        a = s.createElement(o),
+            m = s.getElementsByTagName(o)[0];
+        a.async = 1;
+        a.src = g;
+        m.parentNode.insertBefore(a, m)
+    })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
+    ga('create', 'UA-46470654-3', 'auto');
+    ga('send', 'pageview');
 })
